650b8b79b443810fcd8a6fef8d61e46653614cda
TYLER BENNETT <tyler.bennett52@gmail.com>
2023-01-13 04:25:02 -0600

    personalize skills and icons

--- src/components/About.js
+++ src/components/About.js
@@ -31,15 +31,7 @@ class About extends Component {
                     alt="Avatar placeholder"
                   />
                   <Icon
-                    icon={angularIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
-                  />
-                  <Icon
                     icon={reactIcon}
-                    style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
-                  />
-                  <Icon
-                    icon={vueIcon}
                     style={{ fontSize: "400%", margin: "9% 5% 0 5%" }}
                   />
                 </span>
